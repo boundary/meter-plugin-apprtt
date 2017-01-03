f8b39dce49c285a8a5f0dd55a9e6933867aa3b37
vivek kumar tiwari <vivekkumar_tiwari@bmc.com>
2017-01-03 17:11:17 +0530

    refactoring and comments

--- init.lua
+++ init.lua
@@ -18,7 +18,8 @@ local hostName =nil
 local pollers = nil
 local plugin = nil
 
-local createOptions=function(item)
+-- Create an options object containing required parameters and values
+local function createOptions(item)
    local options = {}
    options.source = notEmpty(item.source,hostName)
    options.filter = item.filter
@@ -26,40 +27,50 @@ local createOptions=function(item)
    return options
 end
 
-local createDataSource = function(item)
+-- Create data source 
+local function createDataSource(item)
     local options = createOptions(item)
     return AppRTTDataSource:new(options)
 end
 
-local createPoller=function(params)
-      local cs = createDataSource(params)
-      local poller= DataSourcePoller:new(notEmpty(tonumber(params.pollInterval),5000), cs)
-      return poller;
+-- Create Poller
+local function createPoller(params)
+    local cs = createDataSource(params)
+    local poller= DataSourcePoller:new(notEmpty(tonumber(params.pollInterval),5000), cs)
+    return poller;
 end
 
-local ck = function()
-end
-local socket1 = net.createConnection(9192, '127.0.0.1', ck)
-socket1:write('{"jsonrpc":"2.0","id":3,"method":"get_system_info","params":{}}')
-   socket1:once('data',function(data)
-     local sucess,  parsed = parseJson(data)
-       hostName =  parsed.result.hostname--:gsub("%-", "")
-       socket1:destroy()
-       pollers = createPoller(params)
-       plugin = Plugin:new(params, pollers)
+-- Removed call back function to be passed in net.createConnection 
+-- passing nil in place of callBack as of now
+-- local ck = function()
+-- end
+-- local socket = net.createConnection(9192, '127.0.0.1', ck)
+-- Create a socket connection to make JSON RPC call for getting system information
+-- This will get called on intallation of plugin
+local socket = net.createConnection(9192, '127.0.0.1', nil)
+socket:write('{"jsonrpc":"2.0","id":3,"method":"get_system_info","params":{}}')
+
+-- once using JSON RPC we get the hostname 
+-- Set the hostname to options, create pollers, datasource and run the plugin  
+socket:once('data',function(data)
+  local sucess,  parsed = parseJson(data)
+  hostName =  parsed.result.hostname--:gsub("%-", "")
+  socket:destroy()
+  pollers = createPoller(params)
+  plugin = Plugin:new(params, pollers)
     
-       function plugin:onParseValues(data, extra)
+  function plugin:onParseValues(data, extra)
 
-            local measurements = {}
-            local measurement = function (...)
-                ipack(measurements, ...)
-            end
-            for K,V  in pairs(data) do
-                measurement(V.metric, V.value, V.timestamp, V.source)
-            end
+    local measurements = {}
+    local measurement = function (...)
+      ipack(measurements, ...)
+    end
+    for K,V  in pairs(data) do
+      measurement(V.metric, V.value, V.timestamp, V.source)
+    end
 
-         return measurements
-       end
-        plugin:run()
+    return measurements
+  end
+  plugin:run()
 end)
 
