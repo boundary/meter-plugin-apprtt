980807f3a7f534ba8fe06058109a3d9f83fb3cf2
vivek tiwari <vitiwari@IN-vitiwari-W1.adprod.bmc.com>
2017-02-24 11:40:59 +0530

    Removed datasource from framework and added to init.lua

--- init.lua
+++ init.lua
@@ -1,11 +1,11 @@
--- @vitiwari
+--@vitiwari
 -- plugin to track App Round Trip Time using lua
 local framework = require('framework')
 local net = require('net')
 local json = require('json')
-
+local clone = framework.table.clone
 local Plugin = framework.Plugin
-local AppRTTDataSource = framework.AppRTTDataSource
+local DataSource = framework.DataSource
 local DataSourcePoller = framework.DataSourcePoller
 local PollerCollection = framework.PollerCollection
 local ipack = framework.util.ipack
@@ -17,6 +17,89 @@ local params = framework.params
 local hostName =nil
 local pollers = nil
 local plugin = nil
+
+
+-- vitiwari
+-- This datasource is to get Average TCP Round Trip Time
+ local AppRTTDataSource = DataSource:extend()
+
+-- AppRTTDataSource
+-- @name ProcessCpuDataSource:new
+-- @param params a table with the configuraiton parameters.
+function AppRTTDataSource:initialize(params)
+  local options = params or {}
+  self.options = options
+  self.items = {}
+  self.count = 0
+  local ck = function()
+  end
+  local socket1 = net.createConnection(8140, '127.0.0.1', ck)
+  local subsMsg="{\"subscriber\" : \""..options.source.."\", \"flow\":{\"filter\":{\"options\":{\"include_loopback\":true}, \"filters\":[\"tcp\"]}}}"
+  socket1:write(subsMsg)
+  socket1:on('data',function(data)
+      local success, parsed = parseJson(data)
+      if not success then
+       self:emit('error', '[Not valid JSON] The subscription flow response can not be parsed. Please report  ')
+      end
+
+      if parsed.error then
+       self:emit('error', parsed.error..", Please delete the plugin and install again with correct parameters.")
+       socket1:destroy()
+      elseif parsed.flows then
+        for k, v in pairs(parsed.flows) do
+          if v.aRtt ~= 0 then
+            AppRTTDataSource:add(v.aRtt)
+          end
+        end
+      else
+        self:emit('error', "Plugin is not working as the subscription flow response syntax has changed, Please report ")
+       socket1:destroy()
+      end
+  end)
+
+
+  socket1:on('error', function (err)
+    self:emit('error', 'There is an issue with socket connection to meter : '.. err.message)
+    socket1:destroy()
+  end)
+end
+
+
+function AppRTTDataSource:add( value)
+      self.count = self.count + 1
+      self.items[self.count] = value
+ end
+
+ function AppRTTDataSource:averageTillNow()
+      local llist=clone(self.items);
+      self.items = {}
+      self.count = 0
+      local sum = 0
+      local count = 0
+      if llist then
+       for k, v in pairs(llist) do
+        sum=sum + v
+        count=count + 1
+       end
+      end
+      local avg = 0
+      if count > 0 then
+       avg= sum/count
+      end
+      return avg
+ end
+
+--AppRTTDataSource fetch function
+function AppRTTDataSource:fetch(context, callback,params)
+  local options = clone(self.options)
+  local avg = AppRTTDataSource:averageTillNow()
+  local result ={}  
+  table.insert(result, {metric = "TCP_RTT", value = avg , source = options.source }) 
+  callback(result)
+end
+
+
+
 
 -- Create an options object containing required parameters and values
 local function createOptions(item)
